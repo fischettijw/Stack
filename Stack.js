4cdd26b69a28c7970786f799eb9417f2d8e1c9dc
Joseph Fischetti <fischettijw@gmail.com>
2021-01-31 13:17:44 -0500

    initial

---
+++ Stack.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value, nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
+export class Stack {
+    constructor() {
+        this.top = null;
+    }
+
+    push(value) {
+        console.log('PUSH');
+
+        let node = new Node(value, this.top);
+        this.top = node;
+
+        this.print();
+    }
+
+    pop() {
+        console.log('POP');
+        let value = null;
+
+        if (this.top) {
+            value = this.top.value;
+            this.top = this.top.nextNode;
+        }
+
+        this.print();
+        return value;
+    }
+
+    print() {
+        let ptr = this.top;
+
+        while (ptr) {
+            console.log(ptr.value);
+            ptr = ptr.nextNode;
+        }
+
+        console.log('---');
+    }
+}
---
+++ StackScript.js
@@ -0,0 +1,14 @@
+import { Stack } from "./Stack.js";
+
+let stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
